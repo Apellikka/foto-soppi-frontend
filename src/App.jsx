7369cc52eb4d8682faee87904d685cb7458155db
unknown <aleksimpellikka@gmail.com>
2024-09-01 23:29:51 +0300

    Implement permanent sidedrawer and search bar to the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,127 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 import "./App.css"
 
-export default function App() {
+const drawerWidth = 240;
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  height: '100%',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    height: '100%', 
+  },
+}));
+
+export default function FotoSoppi() {
   return (
-    <div>
-      <Drawer 
-      PaperProps={{sx: {
-        backgroundColor: "#181818",
-        width: 200
-      }}}
-      variant="permanent">
-        <Box>
-          <List>
-            <ListItemButton>
-              Eka
-            </ListItemButton>
-            <ListItemButton>
-              Toka
-            </ListItemButton>
-          </List>
-        </Box>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{ 
+          width: `calc(100% - ${drawerWidth}px)`, 
+          ml: `${drawerWidth}px`, 
+          bgcolor: "#303030",
+        }}
+      >
+        <Toolbar
+          sx={{
+            px: 0, 
+            height: 64, 
+            display: 'flex',
+            alignItems: 'center',
+            [`@media (min-width: 600px)`]: {
+              paddingLeft: '0px',
+              paddingRight: '0px',
+            },
+          }}
+        >
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box', 
+            bgcolor: "#181818"
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <Toolbar sx={{ color: "#E0E0E0", typography: "h1", fontSize: "2em" }}>Foto-Soppi</Toolbar>
+        <Divider sx={{ bgcolor: "white" }} />
+        <List sx={{ color: "#E0E0E0" }}>
+          {['All', 'Folders', 'Favorites'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton sx={{ ":hover": { bgcolor: "#303030" } }}>
+                <ListItemIcon sx={{ color: "#E0E0E0" }}>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
-    </div>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: '#303030', p: 3, height: "100vh" }}
+      >
+      </Box>
+    </Box>
   );
 }
