add74ebf56d5b51717e12a56b75d2d5613b952f3
unknown <aleksimpellikka@gmail.com>
2024-09-15 18:55:25 +0300

    Generate spring boot project

---
+++ back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/FotoSoppiApplication.java
@@ -0,0 +1,13 @@
+package com.apellikka.fotosoppi.foto_soppi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotoSoppiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotoSoppiApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/apellikka/fotosoppi/foto_soppi/FotoSoppiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apellikka.fotosoppi.foto_soppi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotoSoppiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
