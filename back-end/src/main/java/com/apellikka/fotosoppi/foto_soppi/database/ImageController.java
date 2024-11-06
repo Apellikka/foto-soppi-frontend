d727ef4c5ea82df225eac8c038d3c4fd310f0b5f
unknown <aleksimpellikka@gmail.com>
2024-11-06 21:00:57 +0200

    ImageController stub

--- back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/database/ImageController.java
+++ back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/database/ImageController.java
@@ -1,5 +1,30 @@
 package com.apellikka.fotosoppi.foto_soppi.database;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping(("/images"))
 public class ImageController {
+
+    private final ImageRepository imageRepository;
+
+    @Value("${image.base-directory}")
+    private String baseDirectory;
+
+    public ImageController(ImageRepository imageRepository) 
+    {
+        this.imageRepository = imageRepository;
+    }
+    
+    @GetMapping
+    public Iterable<Image> findAllImages()
+    {
+        return this.imageRepository.findAll(Sort.unsorted());
+    }
     
 }
