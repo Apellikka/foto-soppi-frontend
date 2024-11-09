ab310bbd2502299c2298965581441567df9885ae
unknown <aleksimpellikka@gmail.com>
2024-11-09 21:10:39 +0200

    Add Image class

--- back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/database/Image.java
+++ back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/database/Image.java
@@ -1,8 +1,35 @@
 package com.apellikka.fotosoppi.foto_soppi.database;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "images")
 public class Image {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String path;
+
+    private Image() {}
+
+    public Image(Integer id, String path) 
+    {
+        this.id = id;
+        this.path = path;
+    }
+
+    public Integer getId()
+    {
+        return this.id;
+    }
+
+    public String getPath()
+    {
+        return this.path;
+    }
 }
