b399028678dd479f68ec57e72c5d746f3c5e578c
unknown <aleksimpellikka@gmail.com>
2024-11-16 21:47:04 +0200

    GetImageById RESTful implementation and serve images from /raw endpoint

--- back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/database/ImageController.java
+++ back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/database/ImageController.java
@@ -1,9 +1,10 @@
 package com.apellikka.fotosoppi.foto_soppi.database;
 
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.hateoas.EntityModel;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -12,6 +13,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +23,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -36,43 +43,35 @@ public class ImageController {
         this.imageRepository = imageRepository;
     }
 
-    @GetMapping("/{id}") 
-        public ResponseEntity<Resource> getImageById(@PathVariable Integer id) throws MalformedURLException 
-        {
-            Image image = imageRepository.findById(id)
-                .orElse(null);
-            
-            if(image == null)
-            {
-                System.err.println("ERROR: Image ID not found from DB!");
-                return ResponseEntity.notFound()
-                .header("Message", "Image not found with ID")
-                .build();
-            }
+    @GetMapping("/{id}")
+    public HttpEntity<Image> getImageById(@PathVariable Integer id) throws IOException {
         
-            Path imagePath = Paths.get(baseDirectory, image.getPath());
-            Resource resource = new UrlResource(imagePath.toUri());
-            if(!resource.exists())
-            {
-                System.err.println("ERROR: Image file not found from disk!");
-                return ResponseEntity.notFound().build();
-            }
+        Image image = imageRepository.findById(id).orElse(null);
+        image.add(linkTo(methodOn(ImageController.class).getImageById(id)).withSelfRel());
+        image.add(linkTo(methodOn(ImageController.class).serveImage(id)).withRel("image"));
+        return new ResponseEntity<Image>(image, HttpStatus.OK);
+    }
 
-            String contentType;
-            try {
-                contentType = Files.probeContentType(imagePath);
-                if(contentType == null)
-                {
-                    contentType = "application/octet-stream";
-                }
-            } catch(IOException e) {
-                contentType = "application/octet-stream";
-            }
-
-            return ResponseEntity
-            .ok()
-            .contentType(MediaType.parseMediaType(contentType))
-            .body(resource);
+    @GetMapping("/{id}/raw")
+    public ResponseEntity<Resource> serveImage(@PathVariable Integer id) throws IOException {
+        Image image = imageRepository.findById(id).orElseThrow(() -> 
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Image ID not found from database!"));
+    
+        Path imagePath = Paths.get(baseDirectory, image.getPath());
+        Resource resource = new UrlResource(imagePath.toUri());
+        if (!resource.exists()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Image file not found!");
+        }
+    
+        String contentType = Files.probeContentType(imagePath);
+        if (contentType == null) {
+            contentType = "application/octet-stream"; 
+        }
+    
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(resource);
     }
 
     @GetMapping
