2c63832b9efe22d667f288a1e17fdf66ea6da433
unknown <aleksimpellikka@gmail.com>
2024-11-28 20:02:44 +0200

    Add CORS configuration for app

--- back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/FotoSoppiApplication.java
+++ back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/FotoSoppiApplication.java
@@ -2,11 +2,26 @@ package com.apellikka.fotosoppi.foto_soppi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FotoSoppiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FotoSoppiApplication.class, args);
+	}
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+				registry.addMapping("/images/*")
+					.allowedOrigins("http://localhost:5173")
+					.allowedMethods("GET", "OPTIONS");
+			}
+		};
 	}
 }
