3cb84912f4dcf07a37fc4d84c5d1c636ad48f6da
unknown <aleksimpellikka@gmail.com>
2024-12-18 18:03:56 +0200

    Add component scanning so CustomUserDetailsService is found. Couldn't find it otherwise

--- back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/FotoSoppiApplication.java
+++ back-end/src/main/java/com/apellikka/fotosoppi/foto_soppi/FotoSoppiApplication.java
@@ -3,10 +3,12 @@ package com.apellikka.fotosoppi.foto_soppi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.apellikka.fotosoppi")
 public class FotoSoppiApplication {
 
 	public static void main(String[] args) {
@@ -14,7 +16,7 @@ public class FotoSoppiApplication {
 	}
 
 		@Bean
-		public WebMvcConfigurer corsConfigurer() {
+		WebMvcConfigurer corsConfigurer() {
 			return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
